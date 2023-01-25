535a03c7810efa0910ba3cfcedbdd986f54f417f
Axel20213tn018 <20213tn018@utez.edu.mx>
2023-01-25 09:47:13 -0600

    Implementacion de Firebase

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from '@rneui/themed';
+import { initializeApp } from "firebase/app";
 
 export default function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDVnOc6RZCiAKDQaWUVXYpN9zUwQYNdK-w",
+    authDomain: "finanzas-e70ee.firebaseapp.com",
+    projectId: "finanzas-e70ee",
+    storageBucket: "finanzas-e70ee.appspot.com",
+    messagingSenderId: "935560257918",
+    appId: "1:935560257918:web:af9ca49da4fc6baba9932f"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  
   return (
     <View style={styles.container}>
       <Text>Hola Axel Hernandez</Text>
